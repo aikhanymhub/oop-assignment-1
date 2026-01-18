f16b311a06d162ef39d6aadef8a752d2f296968f
aikhanymsmagulova <253998@astanait.edu.kz>
2026-01-18 15:56:24 +0500

    project3

--- src/DatabaseManager.java
+++ src/DatabaseManager.java
@@ -3,7 +3,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseManager {
-    // Данные из твоего pgAdmin: порт 5433, база car_rental
+
     private static final String URL = "jdbc:postgresql://localhost:5433/car_rental";
     private static final String USER = "postgres";
     private static final String PASSWORD = "1234"; // Тот пароль, который мы установили через ALTER USER
