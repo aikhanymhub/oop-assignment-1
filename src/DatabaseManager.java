36d65f3b66f435ba8a1eabdc740c3f0c932cf5ad
aikhanymsmagulova <253998@astanait.edu.kz>
2026-01-19 17:39:18 +0500

    project3

--- src/DatabaseManager.java
+++ src/DatabaseManager.java
@@ -3,11 +3,16 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseManager {
-
-    private static final String URL = "jdbc:postgresql://localhost:5433/car_rental";
+    private static final String URL = "jdbc:postgresql://localhost:5432/car_rental";
     private static final String USER = "postgres";
     private static final String PASSWORD = "1234";
+
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        try {
+            Class.forName("org.postgresql.Driver");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("PostgreSQL Driver not found.", e);
+        }
     }
 }
